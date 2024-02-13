29c6fd2658b23136c5ec4bc5d87ee24e73951279
anup <anupmandal828109@gmail.com>
2024-02-13 23:22:25 +0530

    host react

--- app/serializers/LoginSerializer.py
+++
@@ -1,5 +0,0 @@
-# from rest_framework.serializers import serializers
-from rest_framework import serializers
-class LoginSerializer(serializers.Serializer):
-    phone=serializers.CharField()
-    password= serializers.CharField()
